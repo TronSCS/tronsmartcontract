927bbcc42efe05fe6af094aece3c7d4d5d42c1d0
khanhnd <bestfriendndk@gmail.com>
2018-11-11 11:47:20 +0000

    Change nav work

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <vue-typed-js :loop="true" :strings="['Smart Contract', 'Free', 'Fast']" v-show="currentPage=='PageInteractSmartContract'||currentPage=='PageInteractSmartContractUrl'">
         <div id="main-title"><img src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Tron.network_logo.svg"><p><span class="typing"></span></p></div>
     </vue-typed-js>
-    <VueGroup id="navigator" v-model="currentPage" class="primary top-indicator inline small-indicator" indicator>
-          <VueGroupButton class="round" value="PageInteractSmartContract" icon-left="visibility">Interact</VueGroupButton>
-          <VueGroupButton class="round" value="PageComposeSmartContract" icon-left="code">Compose</VueGroupButton>
-          <VueGroupButton class="round" value="PageVerifySmartContract" icon-left="verified_user">Verify</VueGroupButton>
-          <VueGroupButton class="round" value="PageAccountInfor" icon-left="account_balance_wallet">Account Info</VueGroupButton>
-          <VueGroupButton class="round" value="PageAuthor" icon-left="help">About us</VueGroupButton>
-    </VueGroup>
+    <div id="nav">
+      <a href="#" class="item" :class="{ isActive: (currentPage=='PageInteractSmartContract')}" target="_blank"><VueIcon icon="visibility" class="small"/>Interact</a>
+      <a href="#/compose" class="item" :class="{ isActive: (currentPage=='PageComposeSmartContract')}" target="_blank"><VueIcon icon="code" class="small"/>Compose</a>
+      <a href="#/verify" class="item" :class="{ isActive: (currentPage=='PageVerifySmartContract')}" target="_blank"><VueIcon icon="verified_user" class="small"/>Verify</a>
+      <a href="#/account" class="item" :class="{ isActive: (currentPage=='PageAccountInfor')}" target="_blank"><VueIcon icon="account_balance_wallet" class="small"/>Account Info</a>
+      <a href="#/about" class="item" :class="{ isActive: (currentPage=='PageAuthor')}" target="_blank"><VueIcon icon="help" class="small"/>About me</a>
+    </div>
      <div class="content">
       <router-view/>
     </div>
@@ -50,14 +50,6 @@
         return this.$store.state.alertbox
       }
     },
-    watch: {
-      currentPage: function(val) {
-        console.log(val);
-        if (val != null) {
-          this.$router.push({ name: val });
-        }
-      }
-    },
     methods: {
       ...mapMutations(['closeAlert'])
     }
@@ -85,4 +77,25 @@
   #footer {
     text-align: center;
   }
+  #nav {
+    border-top-left-radius: 17px;
+        border-bottom-left-radius: 17px;
+            border-top-right-radius: 17px;
+    border-bottom-right-radius: 17px;
+    padding: 5px;
+    margin: 0px auto;
+    background: #ffffff1c;
+    width: fit-content;
+}
+.item {
+    margin: 10px;
+    color:white;
+}
+#nav>a>div {
+    margin: auto 10px;
+    height: 20px;
+}
+.isActive{
+  color:#42ae6b;
+}
 </style>
