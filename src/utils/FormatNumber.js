9a292ac90ef8034ab32c8398f2b6bcbfb4b13324
khanhnd <bestfriendndk@gmail.com>
2018-12-16 13:56:46 +0000

    Moving to private repo

--- src/utils/FormatNumber.js
+++
@@ -1,3 +0,0 @@
-export function FormatNumber(input){
-    return input.toFixed(1).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-}
--- src/utils/Tron.js
+++
@@ -1,3 +0,0 @@
-export function getTronExplorer() {
-    return window.tronWeb.eventServer.host.indexOf("shasta") > 0 ? "https://explorer.shasta.trongrid.io" : "https://tronscan.org/#";
-}
